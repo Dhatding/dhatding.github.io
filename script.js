cd1226ffa2b3976d7a557fae7d6bbb5d66363e53
Dhatding <colin.rohs@gmail.com>
2026-02-18 10:47:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -431,7 +431,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (forward_finish) {
         forward_finish.onclick = function() {
             if (activation_listening) {
-                window.location.href = "../finish.html";
+                window.location.href = "finish.html";
             }
             else {
                 alert("Geben Sie die richitge Anzahl Fehler ein und warten Sie bis das Hörverständnis fertig ist.");
@@ -1748,4 +1748,5 @@ if (document.querySelector(".qlistening")) {
             }
         }
     })
+
 }
